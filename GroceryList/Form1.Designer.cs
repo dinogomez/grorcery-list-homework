365d4a170f1edbf092d9ccca5f0f9bc8248fcc2a
dinogomez <dinogomez117@gmail.com>
2021-07-10 01:45:57 +0800

    Added Initial Project Files

---
+++ GroceryList/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace GroceryList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBoxSell = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstBoxBuy = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstBoxSell
+            // 
+            this.lstBoxSell.FormattingEnabled = true;
+            this.lstBoxSell.Items.AddRange(new object[] {
+            "Milk",
+            "Bread",
+            "Wine",
+            "Cheese",
+            "Bacon"});
+            this.lstBoxSell.Location = new System.Drawing.Point(12, 36);
+            this.lstBoxSell.Name = "lstBoxSell";
+            this.lstBoxSell.Size = new System.Drawing.Size(120, 95);
+            this.lstBoxSell.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sell";
+            // 
+            // lstBoxBuy
+            // 
+            this.lstBoxBuy.FormattingEnabled = true;
+            this.lstBoxBuy.Location = new System.Drawing.Point(245, 36);
+            this.lstBoxBuy.Name = "lstBoxBuy";
+            this.lstBoxBuy.Size = new System.Drawing.Size(120, 95);
+            this.lstBoxBuy.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(155, 46);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(155, 75);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(155, 104);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Price: ";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.ForeColor = System.Drawing.Color.Red;
+            this.lblPrice.Location = new System.Drawing.Point(46, 141);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(58, 25);
+            this.lblPrice.TabIndex = 8;
+            this.lblPrice.Text = "0.00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(283, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Buy";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 185);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstBoxBuy);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstBoxSell);
+            this.Name = "Form1";
+            this.Text = "Grocery List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBoxSell;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstBoxBuy;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GroceryList/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GroceryList
+{
+    public partial class Form1 : Form
+    {
+
+        Dictionary<string, double> itemList = new Dictionary<string, double>(){
+        {"Milk", 25.00},
+        {"Bread", 10.00},
+        {"Wine", 32.00},
+        {"Cheese", 15.00},
+        {"Bacon", 19.99}
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string selectedItem = lstBoxSell.SelectedItem.ToString();
+            lstBoxBuy.Items.Add(selectedItem);
+            double price = Convert.ToDouble(lblPrice.Text) +  itemList[selectedItem];
+            lblPrice.Text = String.Format("{0:0.00}", price);
+        }
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+             //Checks for Empty List Selection
+            if(lstBoxBuy.SelectedItem != null)
+            {
+                string selectedItem = lstBoxBuy.SelectedItem.ToString();
+                lstBoxBuy.Items.Remove(selectedItem);
+                double price = Convert.ToDouble(lblPrice.Text) - itemList[selectedItem];
+                lblPrice.Text = String.Format("{0:0.00}", price);
+            }  
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstBoxBuy.Items.Clear();
+            lblPrice.Text = String.Format("{0:0.00}", 0);
+        }
+    }
+}
